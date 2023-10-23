b96e5e88aad06b92b35bf0af8fea300cec953172
othmane <144280859+OthmaneAssadi212@users.noreply.github.com>
2023-10-23 22:50:47 +0100

    added Exercices

---
+++ ex13/convert-xy.js
@@ -0,0 +1,14 @@
+const  convert = (x, y) => {
+
+  
+    for (var i = 0; i < x.length; i++) {
+      result.push([x[i], y[i]]);
+    }
+  
+    return result;
+  }
+  
+module.exports = convert
+
+
+  
---
+++ ex13/convert-xy.test.js
@@ -0,0 +1,16 @@
+const convert = require("./convert-xy");
+
+describe("convertit deux tableaux de coordonnées x et y en un tableau de coordonnées", () => {
+  it('test 1', () => {
+    expect(convert([0, 2, 5], [8, 6, 9])).toEqual([ [ 0, 8 ], [ 2, 6 ], [ 5, 9 ] ]);
+  });
+
+  it('test 2', () => {
+    expect(convert([2, 2], [4, 8])).toEqual([ [ 2, 4 ], [ 2, 8 ] ]);
+  });
+
+  it('test 3', () => {
+    expect(convert([1], [2])).toEqual([ [ 1, 2 ] ]);
+  });
+});
+
